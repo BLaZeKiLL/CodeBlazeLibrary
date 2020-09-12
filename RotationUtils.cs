fcbd18d6fdbc6f2f2792a6ac37d71ddd02dd8042
Devashish Lal <laldevashish@gmail.com>
2020-09-12 11:31:19 +0530

    release: initial commit

---
+++ RotationUtils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+
+using UniRx;
+
+using UnityEngine;
+
+namespace CodeBlaze.Extensions.UniRx {
+
+    public static class RotationUtils {
+        
+        public static IEnumerator RelativeRotateOverTime(
+            this Transform transform, 
+            Quaternion newRotation,
+            float duration, 
+            IObserver<Unit> observer, 
+            CancellationToken token) 
+            => AbsoluteRotateOverTime(transform, transform.rotation * newRotation, duration, observer, token);
+
+        public static IEnumerator AbsoluteRotateOverTime(
+            this Transform transform, 
+            Quaternion newRotation, 
+            float duration, 
+            IObserver<Unit> observer, 
+            CancellationToken token) {
+            
+            var rotation = transform.rotation;
+            var elapsed = 0.0f;
+            
+            if (token.IsCancellationRequested) yield break;
+            
+            while (elapsed < duration) {
+                rotation = Quaternion.Lerp(rotation, newRotation, elapsed / duration);
+                transform.rotation = rotation;
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            transform.rotation = newRotation;
+            observer.OnNext(new Unit());
+            observer.OnCompleted();
+        }
+
+    }
+
+}
