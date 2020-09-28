ddb61fdbff2bb038b70d106ea322224b2af7163f
Devashish Lal <laldevashish@gmail.com>
2020-09-28 22:58:59 +0530

    feat: added lazy object pools and improved current object pool

---
+++ Assets/Examples/Scripts/LibraryExamples/ObjectPoolExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using CodeBlaze.Library.Collections.Pools;
+
+using UnityEngine;
+
+namespace CodeBlaze.Examples.LibraryExamples {
+
+    public class ObjectPoolExample : MonoBehaviour {
+
+        private LazyObjectPool<string> _pool;
+        
+        private void Start() {
+            _pool = new LazyObjectPool<string>(5, index => $"Hello : {index}");
+            
+            Debug.Log(_pool.Claim());
+        }
+
+    }
+
+}
