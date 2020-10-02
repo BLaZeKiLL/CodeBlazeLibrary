55e19acd0f7c826dcb62fc55ac54fabf17244e68
Devashish Lal <laldevashish@gmail.com>
2020-10-02 13:19:31 +0530

    feat: Random Bag collection & tests added (#14)
    * random bag added, packages updated
    
    * package structure fixed
    
    * Object Pool and Random Bag tests added
    
    * idea files removed
    
    * .idea added to ignore and lazy object pool tests added

---
+++ Tests/Editor/Collections/Pools/LazyObjectPoolTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeBlaze.Library.Collections.Pools;
+
+using NUnit.Framework;
+
+namespace CodeBlaze.Library.Tests.Editor.Collections.Pools {
+
+    public class LazyObjectPoolTests {
+
+        [Test]
+        public void ClaimShouldReturnObjectsInFiFoOrder() {
+            var pool = new LazyObjectPool<string>(5, index => $"Hello {index}");
+
+            for (int i = 0; i < pool.Size; i++) {
+                var obj = pool.Claim();
+                Assert.That(obj, Is.EqualTo($"Hello {i}"));
+            }
+        }
+
+    }
+
+}
---
+++ Tests/Editor/Collections/Random/RandomBagTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+using CodeBlaze.Library.Collections.Random;
+
+using NUnit.Framework;
+
+
+namespace CodeBlaze.Library.Tests.Editor.Collections.Random {
+
+    public class RandomBagTests {
+
+        [Test]
+        public void ShouldReturnRandomItemsFromBag() {
+            var elements = new List<string> {"a", "b", "c", "d"};
+            var bag = new RandomBag<string>(elements);
+
+            for (int i = 0; i < elements.Count; i++) {
+                var item = bag.GetItem();
+                Assert.That(elements, Contains.Item(item));
+                elements.Remove(item);
+            }
+        }
+
+    }
+
+}
