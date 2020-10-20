5c4dd5c7bc53fe2875164a4aa9d95d3b100540e4
BLaZeKiLL <blazeingamer10@gmail.com>
2020-10-20 13:15:50 +0530

    singleton behaviour added, ScenePersistenBehaviour removed

---
+++ Runtime/Behaviour/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace CodeBlaze.Library.Behaviour {
+
+    [DefaultExecutionOrder(-100)]
+    public class Singleton<T> : MonoBehaviour where T : Singleton<T> {
+
+        public static T Current { get; private set; }
+
+        [SerializeField] private bool _isPersistant;
+
+        protected virtual void Awake() {
+            if (!Current) {
+                Current = this as T;
+                if (_isPersistant) DontDestroyOnLoad(gameObject);
+            }
+            else Destroy(gameObject);
+        }
+
+    }
+
+}
